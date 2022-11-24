5161a127fcbefdf04a8f32ff2e2b4f86e539b448
BogdanFoca <81586357+Zeyt8@users.noreply.github.com>
2022-11-24 11:05:31 +0200

    Add sims for individual modules

--- RISCV.srcs/sim_1/new/test_riscv_stud.v
+++ RISCV.srcs/sim_1/new/test_riscv_stud.v
@@ -35,7 +35,7 @@ module RISC_V_TB;
          RD_ID,    
          RS1_ID,   
          RS2_ID);    
-  
+         
   always #5 clk=~clk;
   
   initial begin
@@ -53,4 +53,86 @@ module RISC_V_TB;
     #10 reset=1'b0;
     #200 $finish;
   end
+  
+    begin: test_pc
+        reg res, write;
+        reg [31:0] in;
+        wire [31:0] out;
+        PC pc(clk, res, write, in, out);
+        
+        initial begin
+            #5
+            res = 0;
+            write = 1;
+            in = 24;
+            #5
+            write = 0;
+            in = 19;
+            #5
+            res = 1;
+        end
+    end
+    
+    begin: test_inst_mem
+        reg [9:0] address;
+        wire [31:0] out;
+    
+        instruction_memory im(address, out);
+        
+        initial begin
+            #5
+            address = 0;
+            #5
+            address = 1;
+            #5
+            address = 2;
+            #5
+            address = 3;
+        end
+    end
+    
+    begin: test_registers
+        reg reg_write;
+        reg [4:0] read_reg1, read_reg2, write_reg;
+        reg [31:0] write_data;
+        wire [31:0] read_data1, read_data2;
+    
+        registers r(clk, reg_write, read_reg1, read_reg2, write_reg, write_data, read_data1, read_data2);
+        
+        initial begin
+            #5
+            reg_write = 0;
+            write_reg = 0;
+            write_data = 0;
+            read_reg1 = 1;
+            read_reg2 = 2;
+            #5
+            read_reg1 = 5;
+            read_reg2 = 7;
+            #5
+            reg_write = 1;
+            write_reg = 5;
+            write_data = 2;
+        end
+    end
+    
+    begin: test_imm_gen
+        reg [31:0] in;
+        wire [31:0] out;
+    
+        imm_gen ig(in, out);
+        
+        initial begin
+            #5
+            in = 'hd1a02003;
+            #5
+            in = 'h108093;
+            #5
+            in = 'h1905013;
+            #5
+            in = 'h42a223;
+            #5
+            in = 'h4090e63;
+        end
+    end
 endmodule
