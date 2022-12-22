0fe6fb5db02906aa8d41bdd4da018acf027b59c1
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2022-12-22 16:16:49 +0200

    Small changes

--- RISCV.srcs/sources_1/new/IF_ID.v
+++ RISCV.srcs/sources_1/new/IF_ID.v
@@ -1,19 +1,21 @@
 `timescale 1ns / 1ps
 
 module IF_ID(
-input clk, res,
-input [31:0] pc_in, Instruction_IF, IF_ID_Write,
-output reg [31:0] PC_out, Instruction_ID
+input clk, write, res,
+input [31:0] pc_in, instruction_in,
+output reg [31:0] pc_out, instruction_out
 );
 
     always @(posedge clk) begin
        if (res) begin
-           PC_out <= 0;
-           Instruction_ID <= 0;
+           pc_out <= 0;
+           instruction_out <= 0;
        end
        else begin
-	       PC_out <= pc_in;
-	       Instruction_ID <= Instruction_IF;
+            if (write) begin
+                pc_out <= pc_in;
+                instruction_out <= instruction_in;
+            end
 	   end
     end
 
--- RISCV.srcs/sources_1/new/RISC_V.v
+++ RISCV.srcs/sources_1/new/RISC_V.v
@@ -13,23 +13,22 @@ output [31:0] PC_ID,
 output [31:0] INSTRUCTION_ID,
 output [31:0] IMM_ID,
 output [31:0] REG_DATA1_ID, REG_DATA2_ID,
-output RegWrite_ID, MemtoReg_ID, MemRead_ID, MemWrite_ID,
-output [1:0] ALUop_ID,
-output ALUSrc_ID,
-output Branch_ID
+output [2:0] FUNCT3_ID,
+output [6:0] FUNCT7_ID,
+output [6:0] OPCODE,
+output [4:0] RD_ID,
+output [4:0] RS1_ID,
+output [4:0] RS2_ID
 );
 
     wire [31:0] PC_IF, INSTRUCTION_IF;
-
-    wire [2:0] FUNCT3_ID;
-    wire [6:0] FUNCT7_ID;
-    wire [6:0] OPCODE_ID;
-    wire [4:0] RD_ID; 
-    wire [4:0] RS1_ID;
-    wire [4:0] RS2_ID;
+    wire RegWrite_ID, MemtoReg_ID, MemRead_ID, MemWrite_ID,
+    wire [1:0] ALUop_ID;
+    wire ALUSrc_ID;
+    wire Branch_ID;
     
     IF ifModule(clk, reset, PCSrc, PC_Write, PC_Branch, PC_IF, INSTRUCTION_IF);
-    IF_ID ifIdReg(clk, reset, PC_IF, INSTRUCTION_IF, IF_ID_WRITE, PC_ID, INSTRUCTION_ID);
+    IF_ID ifIdReg(clk, reset, IF_ID_WRITE, PC_IF, INSTRUCTION_IF, PC_ID, INSTRUCTION_ID);
     ID idModule(clk, PC_ID, INSTRUCTION_ID, RegWrite_WB, ALU_DATA_WB, RD_WB, IMM_ID, REG_DATA1_ID, REG_DATA2_ID,
                 RegWrite_ID, MemtoReg_ID, MemRead_ID, MemWrite_ID, ALUop_ID, ALUSrc_ID, Branch_ID,
                 FUNCT3_ID, FUNCT7_ID, OPCODE_ID, RD_ID, RS1_ID, RS2_ID);
